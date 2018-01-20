f3ff1402e875f24eb2f6ad7e72da2694405fab81
Takashi Umeno <umeno.takashi@gmail.com>
2018-01-20 23:53:22 +0900

    compile yaf

---
+++ ansible/ELK6/playbooks/scripts/compile_yaf.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget https://tools.netsa.cert.org/releases/yaf-2.9.3.tar.gz
+tar zxvpf yaf-2.9.3.tar.gz
+(cd yaf-2.9.3; ./configure --enable-ndpi --enable-plugins --enable-applabel --enable-p0fprinter --enable-entropy; make; make install)
+rm -r yaf-2.9.3 yaf-2.9.3.tar.gz
+
