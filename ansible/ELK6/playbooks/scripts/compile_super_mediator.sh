21e678219df5b9b576d5f8fc11925bcb02b5733c
Takashi Umeno <umeno.takashi@gmail.com>
2018-02-14 21:13:39 +0900

    change: one liner

--- ansible/ELK6/playbooks/scripts/compile_super_mediator.sh
+++ ansible/ELK6/playbooks/scripts/compile_super_mediator.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-wget https://tools.netsa.cert.org/releases/super_mediator-1.5.3.tar.gz
-tar zxvpf super_mediator-1.5.3.tar.gz
-(cd super_mediator-1.5.3; ./configure; make; make install)
-rm -r super_mediator-1.5.3 super_mediator-1.5.3.tar.gz
+( cd $HOME && wget https://tools.netsa.cert.org/releases/super_mediator-1.5.3.tar.gz && tar zxvpf super_mediator-1.5.3.tar.gz && (cd super_mediator-1.5.3 && ./configure && make && make install) && rm -r super_mediator-1.5.3 super_mediator-1.5.3.tar.gz )
 
 
