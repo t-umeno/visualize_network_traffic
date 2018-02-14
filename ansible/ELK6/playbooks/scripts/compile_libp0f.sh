9e88e77483d0e3fd07c6ad75106898082856b6bc
Takashi Umeno <umeno.takashi@gmail.com>
2018-02-14 21:13:25 +0900

    change: one liner

--- ansible/ELK6/playbooks/scripts/compile_libp0f.sh
+++ ansible/ELK6/playbooks/scripts/compile_libp0f.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-wget  "https://tools.netsa.cert.org/confluence/download/attachments/16547842/p0flib.tgz"
-tar zxvpf p0flib.tgz
-(cd p0flib/libp0f; ./configure; make; make install)
-rm -r p0flib p0flib.tgz
+( cd $HOME && wget "https://tools.netsa.cert.org/confluence/download/attachments/16547842/p0flib.tgz" && tar zxvpf p0flib.tgz && (cd p0flib/libp0f && ./configure &&  make && make install) && rm -r p0flib p0flib.tgz )
