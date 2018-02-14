c00099984e1912f87cfd461784cd5aaa2b0f5f57
Takashi Umeno <umeno.takashi@gmail.com>
2018-02-14 21:13:55 +0900

    change: one liner

--- ansible/ELK6/playbooks/scripts/compile_yaf.sh
+++ ansible/ELK6/playbooks/scripts/compile_yaf.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-wget https://tools.netsa.cert.org/releases/yaf-2.9.3.tar.gz
-tar zxvpf yaf-2.9.3.tar.gz
-(cd yaf-2.9.3; ./configure --enable-ndpi --enable-plugins --enable-applabel --enable-p0fprinter --enable-entropy; make; make install)
-rm -r yaf-2.9.3 yaf-2.9.3.tar.gz
-
+( cd $HOME && wget https://tools.netsa.cert.org/releases/yaf-2.9.3.tar.gz && tar zxvpf yaf-2.9.3.tar.gz && (cd yaf-2.9.3 && ./configure --enable-ndpi --enable-plugins --enable-applabel --enable-p0fprinter --enable-entropy && make && make install) && rm -r yaf-2.9.3 yaf-2.9.3.tar.gz )
