ba8271336078162d6c4f2be150a817463b6b8fc6
Takashi Umeno <umeno.takashi@gmail.com>
2018-02-22 23:33:14 +0900

    change; add interface (default: enp0s3)

--- ansible/ELK6/playbooks/files/yaf.sh
+++ ansible/ELK6/playbooks/files/yaf.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-sudo  LD_LIBRARY_PATH=/usr/local/lib yaf --force-read-all --mac --silk --verbose --in enp0s3  --out 127.0.0.1 --ipfix-port 18000 --ipfix tcp --noerror --plugin-name=/usr/local/lib/yaf/dpacketplugin.la --max-payload 4096 --applabel --p0fprint --live pcap
+INTERFACE="enp0s3"
+if [ $# -ge 1 ] ; then
+    INTERFACE=$1
+fi
+sudo  LD_LIBRARY_PATH=/usr/local/lib yaf --force-read-all --mac --silk --verbose --in ${INTERFACE}  --out 127.0.0.1 --ipfix-port 18000 --ipfix tcp --noerror --plugin-name=/usr/local/lib/yaf/dpacketplugin.la --max-payload 4096 --applabel --p0fprint --live pcap
 
