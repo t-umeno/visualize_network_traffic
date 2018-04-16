38609ff451aac885db613854366a5794ffbc9701
Takashi Umeno <umeno.takashi@gmail.com>
2018-04-16 23:20:41 +0900

    fix: use bash and .

--- ansible/grafana/playbooks/roles/influxdb/scripts/create_influxdb.list.sh
+++ ansible/grafana/playbooks/roles/influxdb/scripts/create_influxdb.list.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-source /etc/lsb-release
+. /etc/lsb-release
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-
-
