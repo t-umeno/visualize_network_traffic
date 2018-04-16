9b8f1c7b5e4085f6343a800f4b628bfd96f8b075
Takashi Umeno <umeno.takashi@gmail.com>
2018-04-17 00:54:22 +0900

    fix: --execute instead of -e

--- ansible/grafana/playbooks/roles/influxdb/scripts/create_influxdb_database.sh
+++ ansible/grafana/playbooks/roles/influxdb/scripts/create_influxdb_database.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-influx -e 'drop database traffic1'
-influx -e 'CREATE DATABASE traffic1'
-influx -e 'CREATE RETENTION POLICY traffic ON traffic1 DURATION 4w REPLICATION 1 DEFAULT'
+influx --execute 'drop database traffic1'
+influx --execute 'CREATE DATABASE traffic1'
+influx --execute 'CREATE RETENTION POLICY traffic ON traffic1 DURATION 4w REPLICATION 1 DEFAULT'
