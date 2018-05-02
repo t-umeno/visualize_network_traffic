a3b8ff32fc49ce92d63cad09cc42e12cfc4a58f5
Takashi Umeno <umeno.takashi@gmail.com>
2018-05-02 20:46:37 +0900

    change: echo "1 5 * * * find ~/yaf/json -mtime 30 -exec rm -f {} \;" >> /tmp/cron.txt

--- ansible/ELK6/playbooks/scripts/cron_curator.sh
+++ ansible/ELK6/playbooks/scripts/cron_curator.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rm -f /tmp/cron.txt
 crontab -l > /tmp/cron.txt
+echo "1 5 * * * find ~/yaf/json -mtime 30 -exec rm -f {} \;" >> /tmp/cron.txt
 echo "2 5 * * * curator ~/.curator/delete_indicies_yaf.yml" >> /tmp/cron.txt
 cat /tmp/cron.txt | crontab
 rm -f /tmp/cron.txt
