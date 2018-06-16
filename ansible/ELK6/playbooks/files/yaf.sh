1a22d19b8761d1ef489137a2b5e7b5dbd23f3a5a
Takashi Umeno <umeno.takashi@gmail.com>
2018-06-16 09:42:14 +0900

    change: exec "sudo ifconfig ${INTERFACE} up"

--- ansible/ELK6/playbooks/files/yaf.sh
+++ ansible/ELK6/playbooks/files/yaf.sh
@@ -3,5 +3,6 @@ INTERFACE="enp0s3"
 if [ $# -ge 1 ] ; then
     INTERFACE=$1
 fi
+sudo ifconfig ${INTERFACE} up
 sudo  LD_LIBRARY_PATH=/usr/local/lib yaf --force-read-all --mac --silk --verbose --in ${INTERFACE}  --out 127.0.0.1 --ipfix-port 18000 --ipfix tcp --noerror --plugin-name=/usr/local/lib/yaf/dpacketplugin.la --max-payload 4096 --applabel --p0fprint --live pcap
 
