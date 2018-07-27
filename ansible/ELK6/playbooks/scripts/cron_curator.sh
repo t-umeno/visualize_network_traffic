8d6931bb77c4ff4d7aa9287692ff7d5b5857b05b
Takashi Umeno <umeno.takashi@gmail.com>
2018-07-27 23:14:16 +0900

    fix: typo

--- ansible/ELK6/playbooks/scripts/cron_curator.sh
+++ ansible/ELK6/playbooks/scripts/cron_curator.sh
@@ -2,6 +2,6 @@
 rm -f /tmp/cron.txt
 crontab -l > /tmp/cron.txt
 echo "1 5 * * * find ~/yaf/json -mtime 30 -exec rm -f {} \;" >> /tmp/cron.txt
-echo "2 5 * * * curator ~/.curator/delete_indicies_yaf.yml" >> /tmp/cron.txt
+echo "2 5 * * * curator ~/.curator/delete_indices_yaf.yml" >> /tmp/cron.txt
 cat /tmp/cron.txt | crontab
 rm -f /tmp/cron.txt
