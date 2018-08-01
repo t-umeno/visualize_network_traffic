bf40b688cff9c6c310d2e6c60801f23a9d3670b4
Takashi Umeno <umeno.takashi@gmail.com>
2018-08-01 23:01:31 +0900

    change: wait other editing crontab

--- ansible/ELK6/playbooks/scripts/cron_curator.sh
+++ ansible/ELK6/playbooks/scripts/cron_curator.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-rm -f /tmp/cron.txt
+while [ -f /tmp/cron.txt ]; do
+    sleep 1
+done
 crontab -l > /tmp/cron.txt
 echo "1 5 * * * find ~/yaf/json -mtime +29 -exec rm -f {} \;" >> /tmp/cron.txt
 echo "2 5 * * * curator ~/.curator/delete_indices_yaf.yml" >> /tmp/cron.txt
