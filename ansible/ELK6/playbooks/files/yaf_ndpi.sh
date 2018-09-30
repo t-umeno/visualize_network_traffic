29d1381b8570ee9decdfbe6ddd918298655f05c0
Takashi Umeno <umeno.takashi@gmail.com>
2018-09-30 23:42:11 +0900

    deleted:    files/yaf_ndpi.sh
    for Ubuntu 18.04 bionic beaver

--- ansible/ELK6/playbooks/files/yaf_ndpi.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sudo  LD_LIBRARY_PATH=/usr/local/lib yaf --force-read-all --mac --silk --verbose --in enp0s3  --out 127.0.0.1 --ipfix-port 18000 --ipfix tcp --noerror --plugin-name=/usr/local/lib/yaf/dpacketplugin.la --max-payload 4096 --applabel --p0fprint --ndpi --live pcap
