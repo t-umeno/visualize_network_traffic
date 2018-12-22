87ab3713551c5213c766a88110a001f3c2ec2884
Takashi Umeno <umeno.takashi@gmail.com>
2018-12-22 17:45:37 +0900

    deleted:    cron_curator.sh

--- ansible/ELK6/playbooks/scripts/cron_curator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-while [ -f /tmp/cron.txt ]; do
-    sleep 1
-done
-crontab -l > /tmp/cron.txt
-echo "2 5 * * * curator ~/.curator/delete_indices_yaf.yml" >> /tmp/cron.txt
-cat /tmp/cron.txt | crontab
-rm -f /tmp/cron.txt
