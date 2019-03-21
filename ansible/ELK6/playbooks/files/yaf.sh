d4d4e2025c1632c59fe45abd27c76158daf83d8d
Takashi Umeno <umeno.takashi@gmail.com>
2019-03-21 23:22:08 +0900

    change: use TCP 4739 for IPfix

--- ansible/ELK6/playbooks/files/yaf.sh
+++ ansible/ELK6/playbooks/files/yaf.sh
@@ -4,5 +4,5 @@ if [ $# -ge 1 ] ; then
     INTERFACE=$1
 fi
 sudo ifconfig ${INTERFACE} up
-sudo  LD_LIBRARY_PATH=/usr/local/lib yaf --force-read-all --mac --silk --verbose --in ${INTERFACE}  --out 127.0.0.1 --ipfix-port 18000 --ipfix tcp --noerror --plugin-name=/usr/local/lib/yaf/dpacketplugin.la --max-payload 4096 --applabel --p0fprint --live pcap
+sudo  LD_LIBRARY_PATH=/usr/local/lib yaf --force-read-all --mac --silk --verbose --in ${INTERFACE}  --out 127.0.0.1 --ipfix-port 4739 --ipfix tcp --noerror --plugin-name=/usr/local/lib/yaf/dpacketplugin.la --max-payload 4096 --applabel --p0fprint --live pcap
 
