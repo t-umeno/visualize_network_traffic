243aa8b3f43d42880a56dcad98634eaea2908c2b
Takashi Umeno <umeno.takashi@gmail.com>
2019-07-08 14:27:01 +0900

    chante: use config_opendistro.yml

--- ansible/ELK6/playbooks/roles/opendistro/scripts/user.yaf.sh
+++ ansible/ELK6/playbooks/roles/opendistro/scripts/user.yaf.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+LOGSTASH_USER=$1
+LOGSTASH_PASSWORD=$2
 CONTAINER_ID=$(docker ps|grep odfe-node1|cut -d' ' -f1)
-HASH=$(docker exec ${CONTAINER_ID} /bin/sh /usr/share/elasticsearch/plugins/opendistro_security/tools/hash.sh -p yaf123)
-curl -XPUT https://localhost:9200/_opendistro/_security/api/user/yaf -u admin:admin -k -H 'Content-Type: application/json' -d '{    "hash" : "'${HASH}'",    "backend_roles" : [      "yaf"    ],    "attributes" : { },    "description" : "yaf user"}'
+HASH=$(docker exec ${CONTAINER_ID} /bin/sh /usr/share/elasticsearch/plugins/opendistro_security/tools/hash.sh -p ${LOGSTASH_PASSWORD})
+curl -XPUT https://localhost:9200/_opendistro/_security/api/user/yaf -u admin:admin -k -H 'Content-Type: application/json' -d '{    "hash" : "'${HASH}'",    "backend_roles" : [      "'${LOGSTASH_USER}'"    ],    "attributes" : { },    "description" : "yaf user"}'
