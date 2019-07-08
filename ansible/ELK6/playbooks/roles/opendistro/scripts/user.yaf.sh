c783cdba67d93b81a338be24d437503bd9700ca8
Takashi Umeno <umeno.takashi@gmail.com>
2019-07-08 12:46:51 +0900

    change: calculate hash
    exec /usr/share/elasticsearch/plugins/opendistro_security/tools/hash.sh

--- ansible/ELK6/playbooks/roles/opendistro/scripts/user.yaf.sh
+++ ansible/ELK6/playbooks/roles/opendistro/scripts/user.yaf.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -XPUT https://localhost:9200/_opendistro/_security/api/user/yaf -u admin:admin -k -H 'Content-Type: application/json' -d '{    "hash" : "$2y$12$.wnFR4cb9NraGKZ9X8v82OP7IbdYOivWmheY/Fd/Ob7uP1xbA1JhS",    "backend_roles" : [      "yaf"    ],    "attributes" : { },    "description" : "yaf user"}'
+CONTAINER_ID=$(docker ps|grep odfe-node1|cut -d' ' -f1)
+HASH=$(docker exec ${CONTAINER_ID} /bin/sh /usr/share/elasticsearch/plugins/opendistro_security/tools/hash.sh -p yaf123)
+curl -XPUT https://localhost:9200/_opendistro/_security/api/user/yaf -u admin:admin -k -H 'Content-Type: application/json' -d '{    "hash" : "'${HASH}'",    "backend_roles" : [      "yaf"    ],    "attributes" : { },    "description" : "yaf user"}'
