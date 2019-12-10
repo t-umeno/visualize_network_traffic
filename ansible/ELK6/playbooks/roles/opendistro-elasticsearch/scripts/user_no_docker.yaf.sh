3183bbf11e9e026d9e14282e65fab383063bd772
Takashi Umeno <umeno.takashi@gmail.com>
2019-12-10 22:49:34 +0900

    fix: curl --noproxy localhost

--- ansible/ELK6/playbooks/roles/opendistro-elasticsearch/scripts/user_no_docker.yaf.sh
+++ ansible/ELK6/playbooks/roles/opendistro-elasticsearch/scripts/user_no_docker.yaf.sh
@@ -3,4 +3,4 @@ export JAVA_HOME=/usr
 LOGSTASH_USER=$1
 LOGSTASH_PASSWORD=$2
 HASH=$(/bin/bash /usr/share/elasticsearch/plugins/opendistro_security/tools/hash.sh -p ${LOGSTASH_PASSWORD})
-curl -XPUT https://localhost:9200/_opendistro/_security/api/user/yaf -u admin:admin -k -H 'Content-Type: application/json' -d '{    "hash" : "'${HASH}'",    "backend_roles" : [      "'${LOGSTASH_USER}'"    ],    "attributes" : { },    "description" : "yaf user"}'
+curl --noproxy localhost -XPUT https://localhost:9200/_opendistro/_security/api/user/yaf -u admin:admin -k -H 'Content-Type: application/json' -d '{    "hash" : "'${HASH}'",    "backend_roles" : [      "'${LOGSTASH_USER}'"    ],    "attributes" : { },    "description" : "yaf user"}'
