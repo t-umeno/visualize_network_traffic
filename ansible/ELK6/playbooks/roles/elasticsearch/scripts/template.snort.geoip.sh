31025cf17f9031e67e57d204a78aa4dc64fe4808
Takashi Umeno <umeno.takashi@gmail.com>
2019-12-20 16:41:15 +0900

    new file:   template.snort.geoip.sh

---
+++ ansible/ELK6/playbooks/roles/elasticsearch/scripts/template.snort.geoip.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+curl -XPUT http://localhost:9200/_template/snort -H 'Content-Type: application/json' -d '
+{
+  "template" : "snort.*",
+  "mappings" : {
+    "properties" : {
+      "src.geoip" : {
+        "dynamic" : true,
+        "properties" : {
+          "ip" : {
+            "type" : "ip"
+          },
+          "latitude" : {
+            "type" : "half_float"
+          },
+          "location" : {
+            "type" : "geo_point"
+          },
+          "longitude" : {
+            "type" : "half_float"
+          }
+        }
+      },
+      "dst.geoip" : {
+        "dynamic" : true,
+        "properties" : {
+          "ip" : {
+            "type" : "ip"
+          },
+          "latitude" : {
+            "type" : "half_float"
+          },
+          "location" : {
+            "type" : "geo_point"
+          },
+          "longitude" : {
+            "type" : "half_float"
+          }
+        }
+      }
+    }
+  }
+}'
